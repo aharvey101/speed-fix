3aa55ac522f782a2431b68119e150d0dd223e57a
Alexander Harvey <harvey.alex+github@gmail.com>
2019-12-30 14:24:29 +0800

    building home page template to add to all other pages

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const express = require('express'),
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('/public'));
 app.use(methodOverride('_method'));
 
 
@@ -17,6 +17,18 @@ app.use(methodOverride('_method'));
 app.get('/', (req, res) => {
     res.render('home')
 });
+
+app.get('/services', (req, res) => {
+    res.render('services')
+});
+app.get('/contracts', (req, res) => {
+    res.render('contracts')
+});
+app.get('/contact', (req, res) => {
+    res.render('contact')
+});
+
+
 
 
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
