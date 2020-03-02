23ca5acbd7f7d44dbd82da91e10a66ed66bfeae5
Alexander Harvey <harvey.alex+github@gmail.com>
2020-03-02 12:42:10 +0700

    fixed nav and other things

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const express = require('express'),
 
     app = express();
 
-
-
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
